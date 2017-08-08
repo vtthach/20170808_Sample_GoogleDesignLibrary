b0ed8aa03e48581a738bf3e5a25ff0cff64f2333
ThachVo <thach.vo@hubcba.com>
2017-08-08 14:07:47 +0700

    Initial commit

---
+++ app/src/main/java/com/sf0404/a20170808_sample_googledesignlibrary/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.sf0404.a20170808_sample_googledesignlibrary
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun goToTabLayoutActivity(view: View){
+        TabLayoutActivityJava.showMe(this)
+    }
+}
---
+++ app/src/test/java/com/sf0404/a20170808_sample_googledesignlibrary/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.sf0404.a20170808_sample_googledesignlibrary
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
